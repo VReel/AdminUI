b99cb35ce7a9e36550682e7d2813f832fe6f8512
Dan Singerman <dansingerman@gmail.com>
2017-03-20 15:19:35 +0000

    redirect if no user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :redirect_anonymous
 
   layout :layout_by_resource
 
@@ -17,5 +18,12 @@ class ApplicationController < ActionController::Base
     else
       'anonymous_application'
     end
+  end
+
+  def redirect_anonymous
+    return if current_user
+    return if request.path == sessions_path
+
+    redirect_to sessions_path
   end
 end
